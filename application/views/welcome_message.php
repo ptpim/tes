01a6b5fbe2fc18df05c7054bef23fc8af07ed8f9
IT <IT@NB-IT-Yusa>
2015-03-04 11:19:57 +0700

    Testing

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -68,10 +68,10 @@
 
 <div id="container">
 	<h1>Welcome to CodeIgniter!</h1>
-
+            
 	<div id="body">
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
+                    dsadsafdsfsdfsd
 		<p>If you would like to edit this page you'll find it located at:</p>
 		<code>application/views/welcome_message.php</code>
 
